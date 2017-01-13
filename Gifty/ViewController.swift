574283de37627d5311163285cf0f7b76e24a73bc
Lizeth OLARTE <lizeth.olarte@viseo.com>
2017-01-13 17:51:02 +0100

    initial project Gifty iOS

--- Gifty/ViewController.swift
+++ Gifty/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  @IBOutlet weak var profileImage: UIImageView!
+  @IBOutlet weak var giftTableView: UITableView!
+  @IBOutlet weak var profileName: UILabel!
+  
+  let data = ["informatique", "musique", "cadeaux"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    //init profile name and photo
+    profileName.text = "LizethO"
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "giftCellView", for: indexPath)
+    tableViewCell.textLabel?.text = data[indexPath.row].firstLetterCapitalized()
+    
+    return tableViewCell
+    
+    
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data.count
+  }
+  
+}
+
+extension String{
+  func firstLetterCapitalized() -> String {
+    return String(describing: self.uppercased().characters.first!) + String(self.lowercased().characters.dropFirst())
+    
+  }
+}
